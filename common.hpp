819b425586cc724dead289f5a8ffb6d6d598b258
natewind <sdksing@gmail.com>
2018-04-11 22:16:35 +0300

    Changed file structure; added INSTALL draft

--- common.hpp
+++
@@ -1,7 +0,0 @@
-#include "io.hpp"
-#include "range.hpp"
-#include "string.hpp"
-
-#ifndef PYTHONIZE_MANUALLY
-using namespace pythonize;
-#endif
---
+++ pythonize.hpp
@@ -0,0 +1,7 @@
+#include "pythonize-core/io.hpp"
+#include "pythonize-core/range.hpp"
+#include "pythonize-core/string.hpp"
+
+#ifndef PYTHONIZE_MANUALLY
+using namespace pythonize;
+#endif
