c5f43c9c8f1c590b30df1dd119c757ec9aeb3c9a
natewind <sdksing@gmail.com>
2018-07-02 00:36:33 +0300

    Somewhat implemented lazy input by default (including iterator)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() // Testing
 {
-	float number = lazy::input("Enter float: ");
+	float number = 12;//lazy::input("Enter float: ");
 	auto qwerty = str("Hello World!");
 	str text;
 	print(number, qwerty, 42);
@@ -14,17 +14,10 @@ int main() // Testing
 	for (auto i : range(10))
 		print(i);
 
-	//for (auto c : input())
-	//	print(c);
+	int s = input("Input: ");
+	print(s);
 
 	#ifdef TODO_PROPOSALS
-
-	for (int c : input()) // numbers in line
-	for (char c : input()) // chars in line
-	for (str s : input()) // words in line
-	str s1 = input(); // line
-	int a = input(); // number
-	str s2 = input().split[0]; // word (split = lazy list)
 
 	for (int c : file) // numbers from file
 	for (char c : file) // file char by char
--- pythonized/io.hpp
+++ pythonized/io.hpp
@@ -3,7 +3,8 @@
 
 #include <string>      // std::string
 #include <cctype>      // std::isspace
-#include <istream>     // std::ws
+#include <cstddef>     // std::ptrdiff_t, std::nullptr_t
+#include <istream>     // std::istream, std::ws
 #include <iostream>    // std::cin, std::cout
 #include <stdexcept>   // std::invalid_argument
 #include <string_view> // std::string_view
@@ -12,13 +13,85 @@
 
 namespace py
 {
-	inline str input(std::string_view prompt = "")
+	class istream_iterator
 	{
-		std::cout << prompt;
-		std::string res;
-		std::getline(std::cin, res);
-		return res;
-	}
+		// TODO: Reduce code size (here and in input())
+
+		std::istream &in;
+
+		// TODO: Iterate through chars without reading everything twice!
+		inline void skip_ws()
+		{
+			for (char c; (c = in.peek()) != '\n' && std::isspace(c); in.get());
+		}
+
+	public:
+		constexpr istream_iterator(std::istream &stream) : in(stream) {}
+
+		template <typename T>
+		inline operator T()
+		{
+			T res;
+			in >> res;
+			return res;
+		}
+
+		inline operator str()
+		{
+			str res;
+			in >> res;
+			return res;
+		}
+
+		inline operator char() { return in.get(); }
+
+		using value_type = istream_iterator;
+		using difference_type = std::ptrdiff_t;
+		using pointer = istream_iterator *;
+		using reference = istream_iterator &;
+		using iterator_category	= std::input_iterator_tag;
+
+		inline bool operator==(std::nullptr_t end) const
+		{
+			return in.peek() == '\n';
+		}
+
+		inline bool operator!=(std::nullptr_t end) const
+		{
+			return !(*this == end);
+		}
+
+		constexpr auto operator++() const { return *this; }
+		constexpr auto operator*() const { return *this; }
+
+		// Dummies to satisfy Iterator requirements
+		constexpr auto operator++(int) const { return *this; }
+		constexpr auto operator->() const { return this; }
+	};
+
+	struct input
+	{
+		inline input(std::string_view prompt = "") { std::cout << prompt; }
+		auto begin() { return istream_iterator(std::cin); }
+		auto end() { return nullptr; }
+
+		template <typename T>
+		inline operator T()
+		{
+			T res;
+			std::cin >> res;
+			return res;
+		}
+
+		inline operator str()
+		{
+			str res;
+			std::getline(std::cin, res);
+			return res;
+		}
+
+		class split; // TODO (lazy)
+	};
 
 	template <typename T = char const *, typename... Args>
 	inline void print(const T value = "", const Args... args)
@@ -35,48 +108,6 @@ namespace py
 		}
 
 		else std::cout << '\n';
-	}
-
-	namespace lazy
-	{
-		bool nws_left(std::istream &in)
-		{
-			for (char c; (c = in.peek()) != '\n' && c != EOF; in.get())
-				if (!std::isspace(c))
-					return true;
-			return false;
-		}
-
-		// TODO: char_iterator
-
-		struct input // TODO
-		{
-			inline input(std::string_view prompt = "")
-			{
-				std::cout << prompt;
-			}
-
-			template <typename T>
-			inline operator T()
-			{
-				T res;
-				std::cin >> res;
-				if (nws_left(std::cin))
-					throw std::invalid_argument("T(lazy::input)");
-				return res;
-			}
-
-			inline operator str()
-			{
-				std::string res;
-				std::getline(std::cin, res);
-				return res;
-			}
-
-			// begin()
-			// end()
-			//
-		};
 	}
 }
 
